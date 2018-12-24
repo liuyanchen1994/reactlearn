ff50882386dc0685104fa494a59958fce7c25af7
Damon <282823740@qq.com>
2018-12-24 10:26:12 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
+
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById('root')
+)
+
