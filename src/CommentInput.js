a76c6fc76a7a639293ce65e680bbe1f5c2229ac5
Damon <282823740@qq.com>
2019-01-11 14:34:55 +0800

    test

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class CommentInput extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func
+  }
+
   constructor() {
     super()
     this.state = {
-      username: '小明',
+      username: '',
       content: ''
     }
   }
@@ -15,33 +20,59 @@ class CommentInput extends Component {
     })
   }
 
-  handleContentChange (event) {
+  handleContentChange(event) {
     this.setState({
       content: event.target.value
     })
   }
 
-  handleSubmit () {
+  handleSubmit() {
     if (this.props.onSubmit) {
-      const { username, content } = this.state
-      this.props.onSubmit({username, content})
+      this.props.onSubmit({
+        username: this.state.username,
+        content: this.state.content,
+        createdTime: +new Date()
+      })
     }
     this.setState({ content: '' })
   }
 
+  componentDidMount() {
+    this.textarea.focus();
+  }
+  _saveUsername(username) {
+    localStorage.setItem('username', username)
+  }
+
+  handleUsernameBlur(event) {
+    this._saveUsername(event.target.value)
+  }
+
+  componentWillMount() {
+    this._loadUsername()
+  }
+
+  _loadUsername() {
+    const username = localStorage.getItem('username')
+    if (username) {
+      this.setState({ username })
+    }
+  }
   render() {
     return (
       <div className='comment-input'>
         <div className='comment-field'>
           <span className='comment-field-name'>用户名：</span>
           <div className='comment-field-input'>
-            <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)}/>
+            <input value={this.state.username}
+              onBlur={this.handleUsernameBlur.bind(this)}
+              onChange={this.handleUsernameChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field'>
           <span className='comment-field-name'>评论内容：</span>
           <div className='comment-field-input'>
-            <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
+            <textarea ref={(textarea) => this.textarea = textarea} value={this.state.content} onChange={this.handleContentChange.bind(this)} />
           </div>
         </div>
         <div className='comment-field-button'>
--- src/CommentList.js
+++ src/CommentList.js
@@ -4,10 +4,17 @@ class CommentList extends Component {
     static defaultProps = {
         comments: []
     }
+
+    handleDeleteComment (index) {
+        if (this.props.onDeleteComment) {
+          this.props.onDeleteComment(index)
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+                {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} onDeleteComment={this.handleDeleteComment.bind(this)}/>)}
             </div>
         );
     }
